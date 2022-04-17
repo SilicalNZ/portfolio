4b4dd32b72b639c610db430c98a6c514c0912357
akira <chowakira@gmail.com>
2022-04-17 16:44:33 +0200

    update texts

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   personalInfo: {
-    email: 'chowakira@gmail.com',
-    name: 'Akira Chow',
-    descriptionShort: 'I build mobile apps',
+    email: "chowakira@gmail.com",
+    name: "Akira Chow",
+    descriptionShort: "I build mobile apps",
     descriptionLong:
-      'I’m a software engineer specializing in building and maintaining beautiful and responsive mobile apps.',
+      "I'm a software engineer specializing in building and maintaining, beautiful and responsive, mobile apps.",
   },
 
   metadata: {
@@ -12,14 +12,14 @@ module.exports = {
     lang: "en",
     locale: "en_US",
     url: "akirachow.dev",
-    description: 'Akira Chow is a software engineer specializing in building and maintaining beautiful and responsive mobile apps.',
+    description: "Akira Chow is a software engineer specializing in building and maintaining, beautiful and responsive, mobile apps.",
     title: "Akira Chow's Portfolio"
   },
 
   about: {
     paragraphs: [
-      'Hi! My name is Akira Chow and I enjoy creating mobile apps. My interest in mobile development started back in 2017 after I followed a mobile development course in university. After lots of trial and error I finally got the gist of it and ever since then I was hooked!',
-      'Fast-forward to today I\'ve had the opportunity of working at <a href="https://www.lyceo.nl" target="_blank">an education company</a>, <a href="https://www.rbgroup.aero" target="_blank">start-up for airline crews</a>, and <a href="https://www.cae.com" target="_blank">huge aviation corporation</a>. My main focus these days is building and maintaining an application suite of mobile apps at <a href="https://www.cae.com" target="_blank">CAE</a> for crews, pilots, and operators.',
+      'Hi! My name is Akira Chow and I enjoy creating mobile apps. My interest in mobile development started back in 2017 after I followed a mobile development course in university.',
+      'Fast-forward to today. I\'ve had the opportunity of working at <a href="https://www.lyceo.nl" target="_blank">an education company</a>, <a href="https://www.rbgroup.aero" target="_blank">start-up for airline crews</a>, and a <a href="https://www.cae.com" target="_blank">huge aviation corporation</a>. My main focus these days is building and maintaining an application suite of mobile apps at <a href="https://www.cae.com" target="_blank">CAE</a> for crews, pilots, and operators.',
     ],
     recentSkills: [
       'JavaScript (ES6+)',
@@ -33,11 +33,11 @@ module.exports = {
 
   jobs: [
     {
-      id: 3,
+      id: 4,
       title: 'Software Engineer',
       company: 'CAE',
       location: 'Zoetermeer, Netherlands',
-      range: '2021 - Present',
+      range: 'April 2021 - Present',
       url: 'https://www.cae.com',
       tasks: [
         'Write performant, robust, maintainable code for mobile apps',
@@ -46,15 +46,25 @@ module.exports = {
       ],
     },
     {
-      id: 2,
+      id: 3,
       title: 'Software Engineer',
       company: 'RB Group.aero',
       location: 'Zoetermeer, Netherlands',
-      range: '2019 - 2021',
+      range: 'July 2019 - April 2021',
+      url: 'https://rosterbuster.aero/',
+      tasks: [
+        'Worked with a team of four engineers to build a new mobile app for pilots using React Native',
+      ],
+    },
+    {
+      id: 2,
+      title: 'Intern',
+      company: 'RosterBuster',
+      location: 'Zoetermeer, Netherlands',
+      range: 'February 2019 - June 2019',
       url: 'https://www.rbgroup.aero',
       tasks: [
         'Research suitable hybrid mobile application development frameworks for a new mobile app',
-        'Worked with a team of four engineers to build a new mobile app for pilots using React Native',
       ],
     },
     {
@@ -62,7 +72,7 @@ module.exports = {
       title: 'Junior Developer',
       company: 'Inwijs',
       location: 'Leiden, Netherlands',
-      range: '2017 - 2017',
+      range: 'June 2017 - November 2017',
       url: 'https://www.lyceo.nl',
       tasks: [
         'Build mobile apps using Ionic Angular and PHP Symfony as backend',
@@ -74,11 +84,38 @@ module.exports = {
       title: 'Intern',
       company: 'DeDecaan',
       location: 'Leiden, Netherlands',
-      range: '2017 - 2017',
+      range: 'February 2017 - June 2017',
       url: 'https://www.dedecaan.net',
       tasks: ['Rebuild and modernize parts of an existing website'],
     },
   ],
+
+  featured: [
+    // { 
+    //   external: "", 
+    //   title: "", 
+    //   tech: [], 
+    //   github: "", 
+    //   cover: "", 
+    //   cta: "", 
+    //   html: "" 
+    // },
+  ],
+
+  projects: [
+    // { 
+    //   github: "", 
+    //   external: "", 
+    //   title: "", 
+    //   tech: [], 
+    //   html: "" 
+    // },
+  ],
+
+  contact: {
+    title: "Get In Touch",
+    description: ""
+  },
 
   socialMedia: [
     {
