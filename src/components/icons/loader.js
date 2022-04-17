902f1a2fc810e838c55fb055c93c73a3b27f9996
akira <chowakira@gmail.com>
2022-04-17 13:19:37 +0200

    update logo svg icons

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -2,20 +2,33 @@ import React from 'react';
 
 const IconLoader = () => (
   <svg id="logo" version="1.0" xmlns="http://www.w3.org/2000/svg"
-  width="100.000000pt" height="100.000000pt" viewBox="0 0 100.000000 100.000000"
+  width="100" height="100" viewBox="0 0 100 100"
   preserveAspectRatio="xMidYMid meet" role="img">
     <title>Loader Logo</title>
 
-    <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)"
-     fill="currentColor" stroke="none">
-      <path d="M273 873 l-203 -115 0 -253 0 -253 204 -121 c113 -67 213 -121 224
-      -121 18 0 381 200 416 229 14 12 16 44 14 260 l-3 246 -205 123 c-112 67 -214
-      122 -225 121 -11 0 -111 -52 -222 -116z m335 -341 c45 -121 82 -222 82 -225 0
-      -4 -17 -7 -38 -7 -35 0 -39 3 -52 43 l-15 42 -87 3 -86 3 -14 -43 c-14 -41
-      -17 -43 -58 -46 -38 -3 -42 -1 -35 15 4 10 38 99 75 198 38 99 73 192 79 208
-      8 21 17 27 39 27 28 0 30 -4 110 -218z"/>
-      <path d="M467 551 c-15 -42 -27 -80 -27 -84 0 -4 27 -7 60 -7 l59 0 -30 82
-      c-17 45 -31 83 -32 85 -1 1 -15 -33 -30 -76z"/>
+    <g>
+      <g id="B" transform="translate(11.000000, 5.000000)">
+        <text
+          x="24"
+          y="57"
+          fill="currentColor"
+          fontSize="44px"
+          fontFamily="Calibre, San Francisco, SF Pro Text">
+          A
+        </text>
+      </g>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
     </g>
   </svg>
 );
--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -2,20 +2,29 @@ import React from 'react';
 
 const IconLogo = () => (
   <svg id="logo" version="1.0" xmlns="http://www.w3.org/2000/svg"
-  width="100.000000pt" height="100.000000pt" viewBox="0 0 100.000000 100.000000"
+  width="100" height="100" viewBox="0 0 100 100"
   preserveAspectRatio="xMidYMid meet" role="img">
     <title>Logo</title>
  
-    <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)"
-    fill="currentColor" stroke="none">
-      <path d="M273 873 l-203 -115 0 -253 0 -253 204 -121 c113 -67 213 -121 224
-      -121 18 0 381 200 416 229 14 12 16 44 14 260 l-3 246 -205 123 c-112 67 -214
-      122 -225 121 -11 0 -111 -52 -222 -116z m335 -341 c45 -121 82 -222 82 -225 0
-      -4 -17 -7 -38 -7 -35 0 -39 3 -52 43 l-15 42 -87 3 -86 3 -14 -43 c-14 -41
-      -17 -43 -58 -46 -38 -3 -42 -1 -35 15 4 10 38 99 75 198 38 99 73 192 79 208
-      8 21 17 27 39 27 28 0 30 -4 110 -218z"/>
-      <path d="M467 551 c-15 -42 -27 -80 -27 -84 0 -4 27 -7 60 -7 l59 0 -30 82
-      c-17 45 -31 83 -32 85 -1 1 -15 -33 -30 -76z"/>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <text
+          x="24"
+          y="57"
+          fill="currentColor"
+          fontSize="44px"
+          fontFamily="Calibre, San Francisco, SF Pro Text">
+          A
+        </text>
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+      </g>
     </g>
   </svg>
 );
